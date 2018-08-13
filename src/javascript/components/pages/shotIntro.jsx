e02110b6c53d52fdb5b5a877774fccecf3a76073
Won Hong <whong@outlook.com>
2018-08-13 00:13:40 -0400

    updates introduction page

--- src/javascript/components/pages/shotIntro.jsx
+++ src/javascript/components/pages/shotIntro.jsx
@@ -4,17 +4,21 @@ export default function shortIntro() {
 
   const subject = <div className="short-intro-heading">
     An Independent and Honest Advice
-  </div>
+  </div>;
 
-  const subject = <div className="short-intro-content">
-    An Independent and Honest Advice
-  </div>
+  const content = <div className="short-intro-details">
+    Young, skilled, and English-speaking individuals are looking into options to leave their countries, but there are too many traps and uncertainties. I have been there.
+    <br/> <br/>
+    Hiring an immigration attorney is another challenge. They often give you ambiguous and politically correct answers. Plus, they are incentivized to represent your case. They are not independent.
+    <br/> <br/>
+    Here’s where I come in. I have been there, and I know the struggle and uncertainties. Through my platform, I would like to share my experience. I hope you get there more easily.
+  </div>;
 
   return <div className="short-intro-cover">
-
-    <div></div>
-
-    <div></div>
+    <div className="short-intro-content">
+      {subject}
+      {content}
+    </div>
 
   </div>
 }
